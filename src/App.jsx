9ede25f7c4041e2075eaffdc8106cb27001ed765
AliulhaQ07 <azii7264@gmail.com>
2024-10-07 10:42:35 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/functionalComponents/Header.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/functionalComponents/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <>
+      <div className="h-12 bg-zinc-200 flex items-center justify-center">
+        <h1>PRACTICING REACT</h1>
+      </div>
+    </>
+  );
+};
+export default Header;
